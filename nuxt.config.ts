a4206575e0eb610330093532e152dce259411ded
Alejandro Palarich <duxducisarsen@gmail.com>
2024-01-10 19:33:16 -0300

    bug nuxt, agrego sanctum base con variable de .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,88 +1,88 @@
 export default defineNuxtConfig({
 
-				srcDir: './',
+	srcDir: './',
 
-				devtools: { enabled: false },
+	devtools: { enabled: false },
 
-				vite: {
-								css: {
-												preprocessorOptions: {
-																scss: {
-																				additionalData: '@use "~/assets/_colors.scss" as *;'
-																}
-												}
-								},
-								vue: {
-												customElement: true
-								}
-				},
+	vite: {
+		css: {
+						preprocessorOptions: {
+										scss: {
+														additionalData: '@use "~/assets/_colors.scss" as *;'
+										}
+						}
+		},
+		vue: {
+						customElement: true
+		}
+	},
 
-				modules: [
-								'nuxt-auth-sanctum',
-								'@nuxtjs/tailwindcss',
-								'@pinia/nuxt',
-								'dayjs-nuxt'
-				],
+	modules: [
+		'nuxt-auth-sanctum',
+		'@nuxtjs/tailwindcss',
+		'@pinia/nuxt',
+		'dayjs-nuxt'
+	],
 
-				sanctum: {
-								baseUrl: 'http://localhost',
-								redirectIfAuthenticated: true,
-								redirect: {
-												keepRequestedRoute: true,
-												onLogin: '/',
-												onLogout: '/login',
-												onGuestOnly: '/login',
-								}
-				},
-				
-				tailwindcss: {
-								exposeConfig: true,
-								viewer: true,
-								
-								cssPath: '~/assets/css/tailwind.css',
-								configPath: '~/tailwind.config.js',
-								exposeLevel: 2,
-								config: {},
-								injectPosition: 'first',
-				},
+	sanctum: {
+		baseUrl: process.env.API_URL || 'http://localhost',
+		redirectIfAuthenticated: true,
+		redirect: {
+			keepRequestedRoute: true,
+			onLogin: '/',
+			onLogout: '/login',
+			onGuestOnly: '/login',
+		}
+	},
+	
+	tailwindcss: {
+		exposeConfig: true,
+		viewer: true,
+		
+		cssPath: '~/assets/css/tailwind.css',
+		configPath: '~/tailwind.config.js',
+		exposeLevel: 2,
+		config: {},
+		injectPosition: 'first',
+	},
 
-				css: [
-								'bootstrap/dist/css/bootstrap.css',
-								'/assets/css/main.css',
-								'@fortawesome/fontawesome-svg-core/styles.css'
-				],
+	css: [
+		'bootstrap/dist/css/bootstrap.css',
+		'/assets/css/main.css',
+		'@fortawesome/fontawesome-svg-core/styles.css'
+	],
 
-				build: {
-								// Para que Vue no tire un warn en los iconos
-        transpile: [
-            '@fortawesome/fontawesome-svg-core',
-            '@fortawesome/free-brands-svg-icons',
-												'@fortawesome/free-regular-svg-icons',
-												'@fortawesome/free-solid-svg-icons'
-        ]
-    },
+	build: {
+					// Para que Vue no tire un warn en los iconos
+transpile: [
+'@fortawesome/fontawesome-svg-core',
+'@fortawesome/free-brands-svg-icons',
+									'@fortawesome/free-regular-svg-icons',
+									'@fortawesome/free-solid-svg-icons'
+]
+},
 
-				// Variables por entorno
-				$production: {
-								appId: 8
-				//   routeRules: {
-								// '/**': { isr: true }
-				//   }
-				},
-				$development: {
-				
-				},
+	// Variables por entorno
+	$production: {
+					appId: 8
+	//   routeRules: {
+					// '/**': { isr: true }
+	//   }
+	},
+	$development: {
+	
+	},
 
 
-				//  Variables public se mestra, las demás quedan en backend
-				
-				runtimeConfig: {
-						// The private keys which are only available server-side
-				//   apiSecret: '123',
-						// Keys within public are also exposed client-side
-								public: {
-												appBase: process.env.APP_URL,
-												apiBase: process.env.API_URL,
-								}
-				}
+	//  Variables public se mestra, las demás quedan en backend
+	
+	runtimeConfig: {
+			// The private keys which are only available server-side
+	//   apiSecret: '123',
+			// Keys within public are also exposed client-side
+					public: {
+									appBase: process.env.APP_URL,
+									apiBase: process.env.API_URL,
+					}
+	}
 })
