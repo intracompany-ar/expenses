e6d428aa074e1374034138ceda4e9dcecae3c532
Tu Nombre <tuemail@example.com>
2025-03-08 20:23:27 -0300

    refacto global middlware2

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,9 @@
 import { useAuth } from "~/stores/useAuth";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
+    // https://nuxt.com/docs/guide/directory-structure/middleware#when-middleware-runs
+    if (import.meta.server) return
+
     const auth = useAuth();
     
     if (!auth.user && !auth.isLoading) {
